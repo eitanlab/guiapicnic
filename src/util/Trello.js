c84a2c261ccf88d870ce9c33ca33d8fc4f126231
Eitan Lab <eitanlab@gmail.com>
2019-03-14 02:27:52 -0300

    Modifico el Fetch de Trello

--- src/util/Trello.js
+++ src/util/Trello.js
@@ -1,5 +1,3 @@
-const apiKey = 'https://api.trello.com/1/boards/560bf4298b3dda300c18d09c?fields=name,url&key=0b4e96bcf06446fd74d1646248762b43&token=c6a21a1ada53b90b184fff280dcd23a031084fb0f69bf310997e6adaaa7a996e';
-
 const Trello = fetch(`https://api.trello.com/1/boards/59fcfb42903956a0ce07e4cf`)
   .then( response => {
             return response.json();
